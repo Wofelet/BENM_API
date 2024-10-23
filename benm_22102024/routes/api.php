e22b495f5e9f9c53acc18aca153658b35c20a36f
Briam Ernesto Navarro Menjivar <ia.briam320@ufg.edu.sv>
2024-10-22 19:46:07 -0600

    Fañta api

---
+++ benm_22102024/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\DepartamentoController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
